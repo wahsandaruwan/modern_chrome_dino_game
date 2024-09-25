6d348f325684afac92f39f9b1b68653267ffb70c
wahsandaruwan <wahsandaruwan6@gmail.com>
2024-09-26 01:43:23 +0530

    Added the basic config

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*---------------------Create and configure the canvas---------------------*/
+/*-----------Select the canvas-----------*/
+const canvas = document.querySelector("#dino-game");
+
+/*-----------Obtain 2D context-----------*/
+const ctx = canvas.getContext("2d");
+
+/*-----------Dimensions-----------*/
+const canvasWidth = 800;
+const canvasHeight = 200;
